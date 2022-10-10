8378a025e019919084512f7da180f654736ee7ff
Kofi Sey <mrbennysimpson.kss@gmail.com>
2022-10-10 21:50:21 +0000

    testing all things

--- bestdayever.sh
+++ bestdayever.sh
@@ -5,28 +5,27 @@ compliment=$2
 user=$(whoami)
 date=$(date)
 whereami=$(pwd)
-time=$(morning)
 attribute=$3
 
 
-echo "Gooooooood Moooorrrrning $name"
-sleep 2
-echo "How are you doing, $name?"
-sleep 2
+echo -e "\e[1;31m goooooood Moooorrrrning $name \e[0m"
+sleep 3
+echo -e "\e[1;32m How are you doing, $name?\e[0m"
+sleep 3
 echo "Had a good sleep?"
-sleep 2
+sleep 3
 echo "I want to tell you that you the most beautiful $compliment in the world :)"
-sleep 2
+sleep 3
 echo "You best believe that, $name"
-sleep 2
+sleep 3
 echo "Your $attribute make me love you more"
-sleep 2
-echo "I want you to $attribute all the time"
+sleep 3
+echo "I want you to be $attribute all the time"
 sleep 4
 echo "There, do enjoy the rest of the day!"
-sleep 2
+sleep 3
 echo "Love you, $name!"
-sleep 2
+sleep 3
 echo "Bye!"
 sleep 3
 echo "you are currently logging in $whoami. The today is $date. You are currently inside $whereami"
